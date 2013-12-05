070199a5b26620da29d4006ba296c763134302f5
Mischa Gorinskat <m.gorinskat@gmail.com>
2013-12-05 10:32:44 +0100

    WOBS-2978: Initial commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Newscoop\TagesWocheMobilePluginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/admin/mobile")
+     * @Template()
+     */
+    public function adminAction(Request $request)
+    {
+    	return array();
+    }
+}
