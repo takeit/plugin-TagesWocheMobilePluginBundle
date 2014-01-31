1aee956d20504d4e0c651c2b4b510848aac2c4d7
Mark Lewis <thnkloud9@gmail.com>
2014-01-31 17:13:45 -0300

    add container to OfflineIssueServer

--- Command/GenerateOfflineIssueCommand.php
+++ Command/GenerateOfflineIssueCommand.php
@@ -34,11 +34,11 @@ class GenerateOfflineIssueCommand extends ContainerAwareCommand
     protected function execute($input, $output)
     {
         if ($input->getArgument('issue') === 'all') {
-            foreach ($this->getHelper('container')->getService('newscoop_tageswochemobile_plugin.mobile.issue')->findAll() as $issue) {
+            foreach ($this->getContainer()->get('newscoop_tageswochemobile_plugin.mobile.issue')->findAll() as $issue) {
                 $this->generateIssue($issue);
             }
         } else {
-            $issue = $this->getHelper('container')->getService('newscoop_tageswochemobile_plugin.mobile.issue')->find($input->getArgument('issue'));
+            $issue = $this->getContainer()->get('newscoop_tageswochemobile_plugin.mobile.issue')->find($input->getArgument('issue'));
             if ($issue !== null) {
                 $this->generateIssue($issue);
             } else {
@@ -56,7 +56,7 @@ class GenerateOfflineIssueCommand extends ContainerAwareCommand
     private function generateIssue(Article $issue)
     {
         try {
-            $this->getHelper('container')->getService('newscoop_tageswochemobile_plugin.mobile.issue.offline')->generateIssue($issue);
+            $this->getContainer()->get('newscoop_tageswochemobile_plugin.mobile.issue.offline')->generateIssue($issue);
         } catch (\Exception $e) {
             print_r($e->getMessage());
             print_r($e->getTraceAsString());
