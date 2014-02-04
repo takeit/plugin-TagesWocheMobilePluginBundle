cbd989ab8b519b46f1884addf863ee1a3fca88fe
Mark Lewis <thnkloud9@gmail.com>
2014-02-03 22:41:16 -0300

    fixes for offline issue generator

--- Mobile/OfflineIssueService.php
+++ Mobile/OfflineIssueService.php
@@ -93,13 +93,9 @@ class OfflineIssueService
     public function generateIssue(Article $issue)
     {
         foreach ($this->config['clients'] as $client) {
-            $toc = $this->fetchJson($this->container->get('zend_router')->assemble(array(
-                'controller' => 'online',
-                'action' => 'toc',
-                'id' => $issue->getNumber(),
-            ), array(
-                'client' => $client,
-            )));
+            $options = $this->container->getParameter('offline');
+            $onlineIssueUrl = $options['site_url'] . 'api/online/toc/' . $issue->getNumber() . '?client=' . $client;
+            $toc = $this->fetchJson($onlineIssueUrl);
 
             $dir = $this->getIssueDir($toc, $client);
             $this->fetchTocImage($toc, 'cover_url', $dir);
