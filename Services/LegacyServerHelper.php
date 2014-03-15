55665758b2da7bc8b906a6f6e4b19f367d5f04a2
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-03-15 17:03:42 +0100

    CS-5242: Fixes use of correct url

--- Services/LegacyServerHelper.php
+++ Services/LegacyServerHelper.php
@@ -11,11 +11,27 @@ namespace Newscoop\TagesWocheMobilePluginBundle\Services;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\Container;
 use Buzz\Browser;
 
 class LegacyServerHelper
 {
-    const URL_LEGACY_SERVER = 'http://agenda.tageswoche.ch/api';
+    const URL_LEGACY_SERVER = 'http://ausgehen.tageswoche.ch/api';
+
+    /**
+     * @var array
+     */
+    private $config = array('authorization' => false);
+
+    /**
+     * Initialize service data
+     *
+     * @param Symfony\Component\DependencyInjection\Container $container
+     */
+    public function __construct($config = array())
+    {
+        $this->config = array_merge($this->config, $config);
+    }
 
     /**
      * Passes through a request to the legacy solution
@@ -27,11 +43,19 @@ class LegacyServerHelper
     public function passthroughRequest(Request $request, $urlSuffix) {
 
         $parameters = $request->query->all();
+        $headers = array();
 
         try {
+            if ($this->config['authorization']) {
+                $headers = array(
+                    'Authorization' => 'Basic '.base64_encode($this->config['credentials'])
+                );
+            }
+
             $buzz = new Browser();
             $legacyUrl = self::URL_LEGACY_SERVER . $urlSuffix .'?'. http_build_query($parameters);
-            $legacyResponse = $buzz->get($legacyUrl);
+            $legacyResponse = $buzz->get($legacyUrl, $headers);
+
         } catch (\Exception $e) {
             return $this->container->get('newscoop_tageswochemobile_plugin.api_helper')->sentError('Request to legacy server failed.', 500);
         }
