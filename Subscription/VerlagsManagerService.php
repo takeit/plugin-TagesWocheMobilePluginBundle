dbaefb65b493ce9e4b9c15d6f75eeb6fe2400a3e
Mischa Gorinskat <m.gorinskat@gmail.com>
2014-05-27 12:14:51 +0200

    Revert "Revert "WOBS-3246: Adds new service and exception for new subscription management service""
    This reverts commit ad5d3f0e5d489d2da803bc866f0982724e2de6c8.

---
+++ Subscription/VerlagsManagerService.php
@@ -0,0 +1,238 @@
+<?php
+/**
+ * @package Newscoop
+ * @author  Mischa Gorinskat <mischa.gorinskat@sourcefabric.org>
+ * @copyright 2014 Sourcefabric o.p.s.
+ */
+
+namespace Newscoop\TagesWocheMobilePluginBundle\Subscription;
+
+use DateTime;
+use SimpleXmlElement;
+use Exception;
+use Guzzle\Http\Exception\RequestException;
+use Newscoop\Entity\User;
+use Newscoop\Http\ClientFactory;
+
+/**
+ * Verlags Manager Service
+ */
+class VerlagsManagerService
+{
+    const CID = 'customer_id';
+    const CUSTOMER_URL = 'https://www.tageswoche.ch/ftp/subscriptions/{customer}.xml';
+    const TEST_URL = 'https://www.tageswoche.ch/ftp/subscriptions/demo/{customer}.xml';
+
+    /**
+     * @var bool
+     */
+    private $testConnection;
+
+    /**
+     * @var array
+     */
+    private $activeStatusCodes = array(1, 2, 3);
+
+    /**
+     * @param Newscoop\Http\ClientFactory $clientFactory
+     */
+    public function __construct(ClientFactory $clientFactory)
+    {
+        $this->clientFactory = $clientFactory;
+    }
+
+    /**
+     * Get customer view
+     *
+     * @param Newscoop\Entity\User $user
+     *
+     * @return Newscoop\TagesWocheMobilePluginBundle\Subscription\CustomerView
+     */
+    public function getView(User $user)
+    {
+        $subscriber = $this->findSubscriber($user);
+        if (!isset($subscriber)) {
+            return new CustomerView();
+        }
+
+        $view = new CustomerView();
+        $view->customer_id = $user->getAttribute(self::CID) ?: $user->getSubscriber();
+        $view->subcodes = $this->getSubcodes($subscriber);
+        $view->customer_id_subcode = in_array($view->customer_id, $view->subcodes);
+        if ($view->customer_id_subcode) {
+            $view->master_id = (string) $subscriber->uniqueId;
+        }
+
+        $validUntil = $this->getMax($subscriber);
+        if ($validUntil) {
+            $view->print_subscription = true;
+            $view->print_subscription_valid_until = $validUntil;
+        }
+
+        return $view;
+    }
+
+    /**
+     * Test if given id is valid customer id
+     *
+     * @param string $cid
+     * @return bool
+     */
+    public function isCustomer($cid)
+    {
+        $subscriber = $this->findByCustomer($cid);
+        return isset($subscriber) && $this->getMax($subscriber) !== null;
+    }
+
+    /**
+     * Find max valid time from active subscriptions
+     *
+     * @param SimpleXmlElement $subscriber
+     * @return DateTime
+     */
+    public function getMax(SimpleXmlElement $subscriber)
+    {
+        $subscriptions = $subscriber->subscriptions->xpath('subscription');
+
+        $statusCodes = $this->activeStatusCodes;
+        $activeSubscriptions = array_filter($subscriptions, function ($subscription) use ($statusCodes) {
+            return in_array((int) $subscription->statusCode, $statusCodes);
+        });
+
+        $dates = array_map(function ($subscription) {
+            $paidUntil = $subscription->xpath('expectedPaidUntil');
+
+            if (empty($paidUntil)) {
+                $paidUntil = $subscription->paidUntil;
+            } else {
+                $paidUntil = array_pop($paidUntil);
+            }
+
+            return DateTime::createFromFormat('dmY', $paidUntil)->format('Y-m-d');
+        }, $activeSubscriptions);
+
+        if (!empty($dates)) {
+            return DateTime::createFromFormat('Y-m-d', max($dates));
+        }
+    }
+
+    /**
+     * Find subscriber info for given user
+     *
+     * @param Newscoop\Entity\User $user
+     * @return SimpleXmlElement
+     */
+    private function findSubscriber(User $user)
+    {
+        if ($user->getAttribute(self::CID)) {
+            return $this->findByCustomer($user->getAttribute(self::CID));
+        } elseif ($user->getSubscriber()) {
+            throw new VerlagsManagerException('Searching by subscriber not supported by Verlags Manager.');
+        }
+    }
+
+    /**
+     * Find subscriber by customer id
+     *
+     * @param string $customer
+     * @return SimpleXmlElement
+     */
+    private function findByCustomer($customer)
+    {
+        return $this->fetchXml(array(
+            self::CUSTOMER_URL,
+            array('customer' => $customer),
+        ));
+    }
+
+    /**
+     * Fetch xml info via client
+     *
+     * @param array $params
+     * @return SimpleXmlElement
+     */
+    private function fetchXml(array $params)
+    {
+        try {
+            $client = $this->clientFactory->getClient();
+            $response = $client->get($params)->send();
+        } catch (RequestException $e) {
+            throw new VerlagsManagerException();
+        }
+
+        if (!$response->isSuccessful()) {
+            throw new VerlagsManagerException();
+        }
+
+        $xml = simplexml_load_string($response->getBody(true));
+        return isset($xml->subscriber) ? $xml->subscriber : null;
+    }
+
+    /**
+     * Test connection
+     *
+     * @return bool
+     */
+    public function testConnection()
+    {
+        if ($this->testConnection === null) {
+            try {
+                $client = new \Zend_Http_Client(self::TEST_URL);
+                $response = $client->request();
+                $this->testConnection = $response->isSuccessful() || $response->getStatus() == 405;
+            } catch (Exception $e) {
+                $this->testConnection = false;
+            }
+        }
+
+        return $this->testConnection;
+    }
+
+    /**
+     * Generate user key
+     *
+     * @param Newscoop\Entity\User $user
+     * @return string
+     */
+    public function generateKey(User $user)
+    {
+        $subscriber = $this->findSubscriber($user);
+        if (!$subscriber) {
+            return;
+        }
+
+        try {
+            $key = md5($user->getId().$user->getEmail().time());
+            $client = $this->clientFactory->getClient();
+            $response = $client->put(array(
+                self::SUBSCRIBER_URL,
+                array(
+                    'subscriber' => (string) $subscriber->subscriberId,
+                    'userkey' => $key,
+                ),
+            ))->send();
+            return $response->isSuccessful() ? $key : null;
+        } catch (Exception $e) {
+            return;
+        }
+    }
+
+    /**
+     * Get subcodes
+     *
+     * @param SimpleXmlElement $subscriber
+     * @return array
+     */
+    private function getSubcodes(SimpleXmlElement $subscriber)
+    {
+        $subcodes = array();
+        for ($i = 1; $i <= 5; $i++) {
+            $key = sprintf("appId%d", $i);
+            if (isset($subscriber->$key)) {
+                $subcodes[] = (string) $subscriber->$key;
+            }
+        }
+
+        return $subcodes;
+    }
+}
