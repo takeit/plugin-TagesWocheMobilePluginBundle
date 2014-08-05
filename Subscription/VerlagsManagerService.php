799e55079c59442934addea4025261dc7d200ac3
Rafał Muszyński <rmuszynski1@gmail.com>
2014-08-05 14:04:40 +0200

    fixed test url to test verlas connection

--- Subscription/VerlagsManagerService.php
+++ Subscription/VerlagsManagerService.php
@@ -24,7 +24,7 @@ class VerlagsManagerService
     const CID = 'customer_id';
     const CUSTOMER_URL = 'https://www.tageswoche.ch/ftp/subscriptions/{customer}.xml';
     const SUBSCRIBER_URL = 'https://abo.tageswoche.ch/dmpro/ws/subscriber/NMBA/{subscriber}{?userkey}';
-    const TEST_URL = 'https://www.tageswoche.ch/ftp/subscriptions';
+    const TEST_URL = 'https://www.tageswoche.ch/ftp/subscriptions/test.xml';
 
     /**
      * @var bool
